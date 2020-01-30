a536ac843eec9b29f25dcc6c09641e1366298475
unknown <eastkim1@hotmail.com>
2020-01-30 11:31:51 -0700

    finishing up

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,32 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Form() {
-  return (
-    <div>
-      <form className="form">
-        <h3 className="header">Team-Builder App</h3>
-        <label className="label">
-          first name: <input className="input" id="fName" type="text" />
-        </label>
-        <label className="label">
-          last name: <input className="input" id="lName" type="text" />
-        </label>
-        <label className="label">
-          email: <input className="input" id="eMail" type="text" />
-        </label>
-        <label className="label">
-          role:{' '}
-          <select className="input" id="role">
-            <option />
-            <option>Frontend Engineer</option>
-            <option>Backend Engineer</option>
-            <option>UX/UI Designer</option>
-            <option>Software Developer</option>
-            <option>Tester</option>
-          </select>
-        </label>
-      </form>
-    </div>
+const Form = (props) => {
+    const [member, setMember] = useState({
+        id: '',
+        name: '',
+         image: '',
+        eMail: '',
+        role: ''
+      })
+      const handleChanges = (e) => {
+        setMember({ ...member, [e.target.name]: e.target.value })
+      }
+      const submitHandler = (e) => {
+        e.preventDefault()
+        const newGuy = {
+          ...member,
+          id: Date.now()
+        }
+        props.addNewMember(newGuy)
+        setMember(newGuy)
+      }
+    return (
+        <div>
+        <form className="form" onSubmit={submitHandler}>
+          <label htmlFor="name">Name: </label>
+          <input
+            className="input"
+            placeholder="Enter Name"
+            id="name"
+            name="name"
+            type="text"
+            value={member.name}
+            onChange={handleChanges}
+          />
+          <label htmlFor="id">ID: </label>
+          <input
+            className="input"
+            placeholder="Enter Member ID"
+            id="id"
+            name="id"
+            type="text"
+            value={member.lName}
+            onChange={handleChanges}
+          />
+          <label className="email">eMail: </label>
+          <input
+            className="input"
+            placeholder="Enter Email"
+            id="email"
+            name="eMail"
+            type="text"
+            value={member.eMail}
+            onChange={handleChanges}
+          />
+          <label className="role">
+            role:{' '}
+            <select
+              className="input"
+              id="role"
+              name="role"
+              value={member.role}
+              onChange={(event) => handleChange(event)}
+              onChange={handleChanges}
+            >
+              <option />
+              <option>Frontend Engineer</option>
+              <option>Backend Engineer</option>
+              <option>UX/UI Designer</option>
+              <option>Software Developer</option>
+              <option>Tester</option>
+            </select>
+          </label>
+          <button className="submit-button">Submit</button>
+          <button className="submit-button">Add</button>
+        </form>
+      </div> 
   )
 }
 export default Form
